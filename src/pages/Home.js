82ff79584a25082d96ebe549d1278de1026e30e1
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-05-02 16:52:53 -0600

    I am adding the final views for editing, deleting, and viewing user data to the user interface.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios, {isAxiosError} from "axios";
+import {Link, useParams} from "react-router-dom";
 
 export default function Home() {
-    
+
         const [users, setUsers] = useState([])
+
+        const {id} = useParams();
 
         useEffect(() =>{
             loadUsers();
@@ -13,6 +16,11 @@ export default function Home() {
             const result = await axios.get("http://localhost:8080/users")
             setUsers(result.data);
         };
+
+        const deleteUser = async (id) => {
+            await axios.delete(`http://localhost:8080/user/${id}`)
+            loadUsers()
+        }
 
     return (
         <div className='container'>
@@ -36,9 +44,9 @@ export default function Home() {
                                 <td>{user.username}</td>
                                 <td>{user.email}</td>
                                 <td>
-                                    <button className="btn btn-primary mx-2">View</button>
-                                    <button className="btn btn-outline-primary mx-2">Edit</button>
-                                    <button className="btn btn-danger mx-2">Delete</button>
+                                    <Link className="btn btn-primary mx-2" to={`/viewuser/${user.id}`}>View</Link>
+                                    <Link to={`/edituser/${user.id}`} className="btn btn-outline-primary mx-2">Edit</Link>
+                                    <button className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>Delete</button>
                                 </td>
                             </tr>
                         ))
